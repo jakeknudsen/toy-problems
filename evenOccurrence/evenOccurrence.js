883437bb1da22bbd87b8854d50b5112b41072619
Jake Knudsen <jakeknudsen@Jakes-MacBook-Pro.local>
2020-02-12 09:44:02 -0700

    add change to return null if no number occurs an even amount of times

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -10,25 +10,25 @@
  * console.log(onlyEven); //  4
 */
 
+
 var evenOccurrence = function(arr) {
-  //   debugger;
-    // Your code here.
-    var obj = {};
-    arr.forEach(function (val) {
-      if (obj[val] !== undefined) {
-        obj[val]++;
-      } else {
-          obj[val] =1;
-      }
-    })
-    for (var key in obj) {
-        if (obj[key] % 2 === 0) {
-            return key;
-            console.log(key);
-        }
+  // Your code here.
+  var obj = {};
+  arr.forEach(function (val) {
+    if (obj[val] !== undefined) {
+      obj[val]++;
+    } else {
+        obj[val] =1;
     }
-  };
+  })
+  for (var key in obj) {
+      if (obj[key] % 2 === 0) {
+          return key;
+      }
+  }
+  return null;
+};
   
   
    var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
-  //  console.log(onlyEven); //  4
+   console.log(onlyEven); //  4
