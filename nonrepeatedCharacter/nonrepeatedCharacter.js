a088df9b9e5452fff9503d5c72a8898bd2a45351
Jake Knudsen <jakeknudsen@Jakes-MacBook-Pro.local>
2020-02-24 09:32:58 -0700

    will find first non-repeating char in a given string

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,27 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  var splitStr = string.split('');
+  var obj = {};
+  var firstChar;
+
+  for (var i = 0; i < splitStr.length; i++) {
+     if (obj[splitStr[i]]) {
+       obj[splitStr[i]]++
+     } else {
+       obj[splitStr[i]] = 1
+     }
+  }
+  
+  for (var  i = 0; i < splitStr.length; i++) {
+    if (obj[splitStr[i]] === 1) {
+      firstChar = splitStr[i]
+      break;
+    }
+  }
+  return firstChar;
 };
+
+// // firstNonRepeatedCharacter('aabbdc')
+// firstNonRepeatedCharacter('aaazcfggg')
+// firstNonRepeatedCharacter('hello my name is jake')
