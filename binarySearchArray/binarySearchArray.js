af76852979ea2269b788ea4de86fb3ae8845f2be
Jake Knudsen <jakeknudsen@Jakes-MacBook-Pro.local>
2020-03-03 10:03:14 -0700

    will mostly complete binary search

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -9,7 +9,33 @@
  * var index = binarySearch([1, 2, 3, 4, 5], 8);
  * console.log(index); // null
  */
-
 var binarySearch = function (array, target) {
-};
-
+    var left = 0;
+    var right = array.length - 1
+    var index; 
+     var search = (arr, target) => {
+       if (left === right) {
+         return null;
+       }
+       index = Math.floor(left + right /2);
+   
+       if (target === arr[index]) {
+         return index
+       }
+   
+       if (target > arr[index]) {
+        
+         left = index - 1;
+       }
+   
+       if (target < arr[index]) {
+         
+         right = index + 1
+       }
+   
+       search(arr, target);
+     }
+     search(array, target)
+     return index;
+   };
+   
