db7e903cd0f2501928eedfa8f73ad1f140c20a3a
Jake Knudsen <jakeknudsen@Jakes-MacBook-Pro.local>
2020-03-27 19:42:27 -0700

    toy problem for linked list intersection

--- linkedListIntersection/linkedListIntersection.js
+++ linkedListIntersection/linkedListIntersection.js
@@ -1,43 +1,61 @@
 /*
-*
-* Linked List Intersection
-*
-* Write a function linkedListIntersection that returns the node 
-* at which the intersection of two linked lists begins, 
-* or null if there is no such intersection.
-* 
-* Example:
-* 
-* Given the following two linked lists list1 and list2, 
-* linkedListIntersection(list1,list2) should return D 
-* as the node of intersection.
-* 
-*    A → B → C
-*             ↘
-*               D → E → F
-*             ↗
-*        X → Y
-* 
-* Given the following two linked lists list1 and list2, 
-* linkedListIntersection(list1,list2) should return NULL 
-* as there is no point of intersection.
-* 
-*    A → B → C → D
-*    X → Y → Z
-* 
-*/
+ *
+ * Linked List Intersection
+ *
+ * Write a function linkedListIntersection that returns the node
+ * at which the intersection of two linked lists begins,
+ * or null if there is no such intersection.
+ *
+ * Example:
+ *
+ * Given the following two linked lists list1 and list2,
+ * linkedListIntersection(list1,list2) should return D
+ * as the node of intersection.
+ *
+ *    A → B → C
+ *             ↘
+ *               D → E → F
+ *             ↗
+ *        X → Y
+ *
+ * Given the following two linked lists list1 and list2,
+ * linkedListIntersection(list1,list2) should return NULL
+ * as there is no point of intersection.
+ *
+ *    A → B → C → D
+ *    X → Y → Z
+ *
+ */
 
 // Helper function (do not edit)
-function Node (val) {
+function Node(val) {
   var obj = {};
   obj.value = val || null;
   obj.next = null;
   return obj;
 }
 
-
-
 function linkedListIntersection(list1, list2) {
-  // TODO: Implement this function!
-}
+  let obj = {};
 
+  while (list1.next) {
+    obj[list1.value] = list1.next.value;
+    list1 = list1.next;
+    if (!list1.next) {
+      obj[list1.value] = null;
+    }
+  }
+
+  while (list2.next) {
+    if (obj[list2.value] === list2.next.value) {
+      return list2.next.value;
+    }
+    list2 = list2.next;
+    if (!list2.next) {
+      if (obj[list2.value] === list2.next.value) {
+        return list2;
+      }
+    }
+  }
+  return null;
+}
