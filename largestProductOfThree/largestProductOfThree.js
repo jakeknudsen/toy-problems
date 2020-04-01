33144ee8ff0bf575c616647b386210edb6c58803
Jake Knudsen <jakeknudsen@Jakes-MacBook-Pro.local>
2020-03-31 19:17:07 -0700

    find largest of positive nums

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,7 +7,26 @@
  * Extra credit: Make your function handle negative numbers.
  */
 
-
 var largestProductOfThree = function(array) {
   // TODO: everything
+  var sortedArr = array.sort((a, b) => {
+    return a - b;
+  });
+
+  //set largest to one for multiplication
+  let largest = 1;
+  let greatest = [];
+
+  //push 3 largest into new arr
+  for (var i = sortedArr.length - 3; i < sortedArr.length; i++) {
+    greatest.push(sortedArr[i]);
+  }
+
+  //multiply largest by each num
+  greatest.forEach(num => {
+    largest *= num;
+  });
+
+  // return largest
+  return largest;
 };
